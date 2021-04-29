54d8a0563a641e5f9e8208a4ac29bb12d3c21ac1
Floyd <74852029+Floyd-ro@users.noreply.github.com>
2021-04-29 16:35:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+song = "";
+leftWristX = "";
+leftWristY= "";
+rightWristX = "";
+rightWristY = "";
+
+function preload()
+{
+    song = loadSound("music.mp3")
+    song = loadSound("music2.mp3")
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = m15.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('Posenet Is Intialized !');
+}
+
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
+
+function play()
+{
+   song.play();
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+    }
+}
